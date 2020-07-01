f3ee2f6488959f61354f7d2f0bf091a28776ade5
ArielJiang0520 <32921432+ArielJiang0520@users.noreply.github.com>
2020-06-30 22:40:38 -0700

    add instructor site poster

--- course_poster.py
+++ course_poster.py
@@ -23,17 +23,31 @@ with open(sys.argv[1]) as fp:
 for host in course_host_config[courseid]:
     if host['enabled']:
         print('Host {} enabled. Address = {}'.format(host['name'],host['host_addr']))
-        for lesson_name in next(os.walk(course_directory))[1]:
-            if '.' not in lesson_name and 'I_' not in lesson_name:
-                print("===Posting ", lesson_name)
-                lesson_directory = course_directory + lesson_name
-                print(lesson_directory)
-                lesson_tag = lesson_name
-                ghost_poster = GhostPoster(course_tag=lesson_tag,
-                                           course_directory=lesson_directory,
-                                           key=host['admin_key'],
-                                           host_addr=host['host_addr'],
-                                           clean_old_lessons=True)
-                ghost_poster.post_course()
+        if not host['instructor']:
+            for lesson_name in next(os.walk(course_directory))[1]:
+                if '.' not in lesson_name and 'I_' not in lesson_name:
+                    print("===Posting ", lesson_name)
+                    lesson_directory = course_directory + lesson_name
+                    print(lesson_directory)
+                    lesson_tag = lesson_name
+                    ghost_poster = GhostPoster(course_tag=lesson_tag,
+                                               course_directory=lesson_directory,
+                                               key=host['admin_key'],
+                                               host_addr=host['host_addr'],
+                                               clean_old_lessons=True)
+                    ghost_poster.post_course()
+        else:
+            for lesson_name in next(os.walk(course_directory))[1]:
+                if '.' not in lesson_name and 'I_' in lesson_name:
+                    print("===Posting ", lesson_name)
+                    lesson_directory = course_directory + lesson_name
+                    print(lesson_directory)
+                    lesson_tag = lesson_name
+                    ghost_poster = GhostPoster(course_tag=lesson_tag,
+                                               course_directory=lesson_directory,
+                                               key=host['admin_key'],
+                                               host_addr=host['host_addr'],
+                                               clean_old_lessons=True)
+                    ghost_poster.post_course()
     else:
         print('Host {} not enabled. Address = {}'.format(host['name'],host['host_addr']))
