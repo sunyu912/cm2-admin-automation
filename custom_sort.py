082acf123999c0ac08a3f230c0e3544ac058d4db
ArielJiang0520 <32921432+ArielJiang0520@users.noreply.github.com>
2020-07-11 14:46:38 -0700

    rewrote custom_sort.py

--- custom_sort.py
+++ custom_sort.py
@@ -1,44 +1,96 @@
 import os
 import re
 
-'''
-see lesson name naming convention
-'''
-# TODO: rewrite custom_sort.py
+# add more patterns according to curriculum development
+patterns = {
+    1: r'L(\d+) ([a-zA-Z ]+) ?(\d+)?\.md',
+    2: r'L(\d+)-(\d+) ?([A-Za-z]+)?\.md',
+    3: r'Question (\d+)\.md',
+    4: r'Lesson Plan ?(\d+)?\.md'
+}
+
 
 def sort_title(filepath) -> int:
-    """
-
-    :param filepath: filepath as string
-    :return: score for sorting
-    """
     _, title = os.path.split(filepath)
-    if 'Overview' in title:
-        return -1e9
-    elif 'Summary' in title:
-        return 1e9
-    elif 'Project' in title:
-        return 1e9 - 1
-    elif 'Homework' in title:
-        return 1e9 - 2
-    elif 'Exercise' in title:
-        return 1e9 - 3
-    elif 'Lesson Plan' in title:
-        match_obj = re.match(r'Lesson Plan (\d+).md', title)
-        if not match_obj:
-            return 0
-        return int(match_obj.group(1))
+    match_obj = None
+    idx = 1
+    while idx <= len(patterns):
+        match_obj = re.match(patterns[idx], title)
+        if match_obj:
+            break
+        idx += 1
+    if not match_obj:
+        return 0
     else:
-        match_obj = re.match(r'L(\d+)(-*)(\d*)( *)(.*)', title)
-        if not match_obj:
-            match_obj = re.match(r'(Question)( )(\d\d)', title)
-            if not match_obj:
-                return 0
-            elif match_obj.group(1) == 'Question':
-                return int(match_obj.group(3))
-            else:
-                return 0
-        if match_obj.group(1) == '0':
+        try:
+            if idx == 1:
+                sort_dict = {
+                    'Setup': -150,
+                    'Overview': -50,
+                    'Project': 0,
+                    'Homework': 50,
+                    'Summary': 150
+                }
+                if match_obj.group(2) in sort_dict:
+                    return sort_dict[match_obj.group(2)] + int(match_obj.group(3)) \
+                        if match_obj.group(3) else sort_dict[match_obj.group(2)]
+                else:
+                    return 0 + int(match_obj.group(3)) if match_obj.group(3) else 0
+            elif idx == 2:
+                return int(match_obj.group(2)) * 2 + 1 if match_obj.group(3) \
+                    else int(match_obj.group(2)) * 2
+            elif idx == 3:
+                return int(match_obj.group(1))
+            elif idx == 4:
+                return int(match_obj.group(1)) if match_obj.group(1) else 0
+        except:
             return 0
-        return int(match_obj.group(3)) * 2 if match_obj.group(5) != 'Exercise.md' \
-            else int(match_obj.group(3)) * 2 + 1
+
+
+# if __name__ == '__main__':
+#     tests = ['L1-1 Exercise.md', 'L1-1.md', 'L1 Overview.md', 'L1 Homework.md',
+#              'L0 Dev Setup.md', 'Question 03.md', 'Lesson Plan 04.md', 'A random title.md',
+#              'Lesson Plan.md']
+#     for t in tests:
+#         print(t)
+#         print(sort_title(t))
+# print(sort_title('Lesson Plan 04.md'))
+
+
+#
+# def sort_title(filepath) -> int:
+#     """
+#
+#     :param filepath: filepath as string
+#     :return: score for sorting
+#     """
+#     _, title = os.path.split(filepath)
+#     if 'Overview' in title:
+#         return -1e9
+#     elif 'Summary' in title:
+#         return 1e9
+#     elif 'Project' in title:
+#         return 1e9 - 1
+#     elif 'Homework' in title:
+#         return 1e9 - 2
+#     elif 'Exercise' in title:
+#         return 1e9 - 3
+#     elif 'Lesson Plan' in title:
+#         match_obj = re.match(r'Lesson Plan (\d+).md', title)
+#         if not match_obj:
+#             return 0
+#         return int(match_obj.group(1))
+#     else:
+#         match_obj = re.match(r'L(\d+)(-*)(\d*)( *)(.*)', title)
+#         if not match_obj:
+#             match_obj = re.match(r'(Question)( )(\d\d)', title)
+#             if not match_obj:
+#                 return 0
+#             elif match_obj.group(1) == 'Question':
+#                 return int(match_obj.group(3))
+#             else:
+#                 return 0
+#         if match_obj.group(1) == '0':
+#             return 0
+#         return int(match_obj.group(3)) * 2 if match_obj.group(5) != 'Exercise.md' \
+#             else int(match_obj.group(3)) * 2 + 1
